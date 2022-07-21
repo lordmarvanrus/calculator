37938fe1b3c4049ad4474ec2f0f9312840792368
Vladimir <marvalo73@gmail.com>
2022-07-21 14:34:47 +0700

    Исправил

--- src/main/java/com/calculator/calculator/vladimir/CalculatorController.java
+++ src/main/java/com/calculator/calculator/vladimir/CalculatorController.java
@@ -1,10 +1,11 @@
 package com.calculator.calculator.vladimir;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController @RequestMapping("/calculator")
 public class CalculatorController {
     private final CalculatorService calculatorService;
 
@@ -12,24 +13,24 @@ public class CalculatorController {
         this.calculatorService = calculatorService;
     }
 
-    @GetMapping( "/calculator")
+    @GetMapping
     public String hello() {
         return calculatorService.hello();
     }
-    @GetMapping( "/calculator/plus")
-    public String plus(@RequestParam ("num1") int num1, @RequestParam ("num2") int num2) {
+    @GetMapping( "/plus")
+    public String plus(@RequestParam (name = "num1", required = false) Integer num1, @RequestParam (name = "num2", required=false) Integer num2) {
         return calculatorService.plus(num1, num2);
     }
-    @GetMapping("/calculator/minus")
-    public String minus(@RequestParam ("num1") int num1, @RequestParam ("num2") int num2) {
+    @GetMapping("/minus")
+    public String minus(@RequestParam (name = "num1", required = false) Integer num1, @RequestParam (name = "num2", required=false) Integer num2) {
         return calculatorService.minus(num1, num2);
     }
-    @GetMapping( "/calculator/multiply")
-    public String multiply(@RequestParam ("num1") int num1, @RequestParam ("num2") int num2) {
+    @GetMapping( "/multiply")
+    public String multiply(@RequestParam (name = "num1", required = false) Integer num1, @RequestParam (name = "num2", required=false) Integer num2) {
         return calculatorService.multiply(num1, num2);
     }
-    @GetMapping( "/calculator/divide")
-    public String divide(@RequestParam ("num1") int num1, @RequestParam ("num2") int num2) {
+    @GetMapping( "/divide")
+    public String divide(@RequestParam (name = "num1", required = false) Integer num1, @RequestParam (name = "num2", required=false) Integer num2) {
         return calculatorService.divide(num1, num2);
     }
 }
--- src/main/java/com/calculator/calculator/vladimir/CalculatorServiceImpl.java
+++ src/main/java/com/calculator/calculator/vladimir/CalculatorServiceImpl.java
@@ -3,26 +3,37 @@ package com.calculator.calculator.vladimir;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CalculatorServiceImpl implements  CalculatorService {
+public class CalculatorServiceImpl implements CalculatorService {
     public String hello() {
         return "Добро пожаловать в калькулятор";
     }
-
-    public String plus(int num1, int num2) {
+    public String plus(Integer num1, Integer num2) {
+        if (num1 == null || num2 == null) {
+            throw new NullPointerException("Не был передан один из параметров");
+        }
         return num1 + " + " + num2 + " = " + (num1 + num2);
     }
 
-    public String minus(int num1, int num2) {
+    public String minus(Integer num1, Integer num2) {
+        if (num1 == null || num2 == null) {
+            throw new NullPointerException("Не был передан один из параметров");
+        }
         return num1 + " - " + num2 + " = " + (num1 - num2);
 
     }
 
-    public String multiply(int num1, int num2) {
+    public String multiply(Integer num1, Integer num2) {
+        if (num1 == null || num2 == null) {
+            throw new NullPointerException("Не был передан один из параметров");
+        }
         return num1 + " * " + num2 + " = " + (num1 * num2);
 
     }
 
-    public String divide(int num1, int num2) {
+    public String divide(Integer num1, Integer num2) {
+        if (num1 == null || num2 == null) {
+            throw new NullPointerException("Не был передан один из параметров");
+        }
         if (num2 == 0) {
             throw new NullPointerException("На ноль делить нельзя!");
         }
